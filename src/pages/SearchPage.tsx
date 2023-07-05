28a6971e71c6d2d5c186585ec91cb217b1208da0
ErickLaporta <erickyanlaportadasilvacontas@gmail.com>
2023-07-05 19:22:12 -0300

    bugfix: req4

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,8 +1,14 @@
 import { useState } from 'react';
 import { NotFoundProducts } from '../components/NotFoundProducts';
+import { getCategories } from '../services/api';
 
-export function SearchPage() {
-  const [product, setProducts] = useState([]); //
+export async function SearchPage() {
+  const [product, setProducts] = useState([]);
+
+  const searchProduct = async () => {
+    const [data] = await getCategories();
+    setProducts(data.name);
+  };
 
   return (
     <>
